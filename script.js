246aa8f07e67504c2650426221e737f81b4d59f3
Felipe SImao <78655591+FelipeSimao@users.noreply.github.com>
2021-11-13 20:01:52 -0300

    Add files via upload
    tecnologias:
    °HTML
    °CSS
    °JS

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var img = document.querySelector('#like')
+
+img.addEventListener('click', like)
+var validation = true
+
+//like
+
+function like(){
+
+    if(validation){
+        img.src = './img/redHeart.png'
+        validation = false
+
+        return
+
+    }else{
+        img.src = './img/purpleHeart.png'
+        validation = true
+    }
+
+}
